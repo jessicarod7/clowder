f30448f3419bb1f4430b750af7098f52f38d1f6d
Pete Savage <psavage@redhat.com>
2024-12-02 19:23:38 +0000

    Moved dir

--- ci/konflux_minikube_e2e_tests.sh
+++ ci/konflux_minikube_e2e_tests.sh
@@ -11,7 +11,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  ./kubectl" | sha256sum --check
 chmod +x kubectl
-mv kubectl /var/workir/bin
+
 export PATH="/var/workdir/bin:$PATH"
 cd /var/workdir/source
 (
